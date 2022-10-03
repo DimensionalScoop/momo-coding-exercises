42850e80c8b602ca41768d07f94019e11722f106
lydiavonderweiden <lydia.weiden@tu-dortmund.de>
2022-10-03 16:52:48 +0200

    day two, challenge 1

---
+++ adventofcode21/momo_d2/diving.py
@@ -0,0 +1,14 @@
+with open("adventofcode21/momo_d2/input") as file:
+    f = [line.split() for line in file.readlines()]
+
+x = 0
+z = 0
+
+for d in f:
+    match d[0]:
+        case 'up': z -= int(d[1])
+        case 'down': z += int(d[1])
+        case 'forward': x += int(d[1])
+
+print(x*z)
+        
