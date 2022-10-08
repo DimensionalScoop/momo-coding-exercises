590fb9cd368e92c54a0ec90a8f9977b06cfdad19
Max Pernklau <max.pernklau@udo.edu>
2022-10-08 16:33:25 +0200

    solve d3.1

---
+++ adventofcode21/max_d3/power.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+
+def iter_as_binary_number(a):
+    chars = [str(i) for i in a]
+    string = "".join(chars)
+    number = int(string, base=2)
+    return number
+
+
+def load():
+    with open("input") as file:
+        chars = [
+            [int(c) for c in line if c in ("0", "1")]
+            for line in file.readlines()
+        ]
+        binary = np.array(chars, dtype="bool")
+    return binary
+
+
+if __name__ == "__main__":
+    binary = load()
+
+    gamma = np.round(binary.mean(axis=0)).astype("int")
+    epsilon = 1 - gamma
+    assert (gamma + epsilon).all()
+
+    gamma = iter_as_binary_number(gamma)
+    epsilon = iter_as_binary_number(epsilon)
+
+    print(gamma * epsilon)
