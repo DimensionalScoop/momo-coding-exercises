0ddf3620958f43c77f4e17925ae376c73d886709
Max Pernklau <max.pernklau@udo.edu>
2022-10-22 19:22:29 +0200

    solve day 4.2

--- adventofcode21/max_d4/bingo.py
+++ adventofcode21/max_d4/bingo.py
@@ -13,6 +13,18 @@ def get_winning_board(random_numbers, boards):
                 return b, n
 
 
+def get_losing_board(random_numbers, boards):
+    calls = []
+    for n in random_numbers:
+        calls.append(n)
+
+        if len(boards) == 1:
+            if has_bingo(boards[0], calls):
+                return boards[0], n
+
+        boards = [b for b in boards if not has_bingo(b, calls)]
+
+
 def has_bingo(board: NDArray, numbers: list):
     calls = np.isin(board, numbers)
     return has_complete_lines(calls) or has_complete_lines(calls.T)
@@ -25,13 +37,19 @@ def has_complete_lines(calls: NDArray):
     return False
 
 
-if __name__ == "__main__":
-    random_numbers, boards = read.get_input()
-    board, last_call = get_winning_board(random_numbers, boards)
-
-    # sum of unmarked numbers
+def score(board, last_call, random_numbers):
     last_call_idx = random_numbers.index(last_call) + 1
     calls = random_numbers[:last_call_idx]
     was_called = np.isin(board, calls)
 
-    print(board[~was_called].sum() * last_call)
+    return board[~was_called].sum() * last_call
+
+
+if __name__ == "__main__":
+    random_numbers, boards = read.get_input()
+
+    board, last_call = get_winning_board(random_numbers, boards)
+    print(f"Winner: {score(board,last_call,random_numbers)}")
+
+    board, last_call = get_losing_board(random_numbers, boards)
+    print(f"Loser: {score(board,last_call,random_numbers)}")
