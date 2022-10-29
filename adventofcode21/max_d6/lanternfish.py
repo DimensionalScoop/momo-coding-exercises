888a6ed6d566f3143ce9ecf82855e60b1d7dd221
Max Pernklau <max.pernklau@udo.edu>
2022-10-30 01:13:10 +0200

    solve d6

---
+++ adventofcode21/max_d6/lanternfish.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+# index is age, value is number of fishies
+count = np.zeros(9, "int64")
+
+fishies = np.genfromtxt("input", "int32", delimiter=",")
+ages, starting_counts = np.unique(fishies, return_counts=True)
+for start_age, start_count in zip(ages, starting_counts):
+    count[start_age] += start_count
+
+
+def step():
+    respawn = count[0]
+    count[:-1] = count[1:]
+    count[-1] = 0
+
+    count[8] += respawn
+    count[6] += respawn
+
+
+for _ in range(256):
+    step()
+
+print(count.sum())
